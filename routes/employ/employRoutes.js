e7b88d299cc45d1992097267f9a23f5b863155d0
Thegeekpod <supriyo7dey@gmail.com>
2023-12-30 21:41:26 +0530

    apijson

--- routes/employ/employRoutes.js
+++ routes/employ/employRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const apiconfig = require('../../apiconfig.json')
 const { addEmploy, employList, updateEmploy, deleteEmployee } = require('../../controllers/employ/employController');
+const { verifyToken } = require('../../utils/authUtils');
 
 const router =express.Router();
 
-router.post(`${apiconfig.API_ENDPOINT.ADD_EMPLOY}`,addEmploy)
-router.post(`${apiconfig.API_ENDPOINT.EMPLOY_LIST}`,employList)
-router.put(`${apiconfig.API_ENDPOINT.EMPLOY_UPDATE}`,updateEmploy)
-router.delete(`${apiconfig.API_ENDPOINT.EMPLOY_DELETE}`,deleteEmployee)
+router.post(`${apiconfig.API_ENDPOINT.ADD_EMPLOY}`,verifyToken, addEmploy)
+router.post(`${apiconfig.API_ENDPOINT.EMPLOY_LIST}`,verifyToken, employList)
+router.put(`${apiconfig.API_ENDPOINT.EMPLOY_UPDATE}`,verifyToken, updateEmploy)
+router.delete(`${apiconfig.API_ENDPOINT.EMPLOY_DELETE}`,verifyToken, deleteEmployee)
 
 
 
